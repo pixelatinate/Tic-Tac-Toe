a6c195c4be6113fd0e58001771872baafa8d437d
Swasti Mishra <swastiy2k1@gmail.com>
2021-01-28 23:23:05 -0500

    Create toe.cpp
    initial commit

---
+++ toe.cpp
@@ -0,0 +1,36 @@
+// lab 1, toe.cpp
+// This lab plays a little game of tic tac toe with you.
+// COSC 140
+// Swasti Mishra
+// 01/24/21
+// Professor Emrich
+// Students:
+
+# include <iostream>
+# include <limits> 
+# include <vector>
+# include <string>
+
+using namespace std ;
+
+int main () {
+
+	unsigned int size ;
+
+	cout << "Enter a grid size (Options: 3, 4, 5) " ;
+	while ( ( size > 5 ) || ( size < 3 ) ) {
+		cin >> size ;
+	}
+	// asking for a grid size and error checking
+
+	char board[ size ][ size ] = { '-' '-' '-' '-' '-' 
+		'-' '-' '-' '-' '-' '-' '-' '-' '-' '-' '-' '-' 
+			----------------------- } ;
+	// builds the board AFTER accepting the size
+
+	for ( unsigned int i = 0 ; i < ( size * size ) ; i++ ) {
+			cout << board << "\n" ;
+	}
+}
+	
+
